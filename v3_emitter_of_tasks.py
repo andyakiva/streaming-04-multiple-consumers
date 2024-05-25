49f0c61cdbba1838e9144d89bf51f30a5bba5618
Andy <andyjasher@gmail.com>
2024-05-24 23:53:20 -0600

    half way done

---
+++ v3_emitter_of_tasks.py
@@ -0,0 +1,82 @@
+"""
+    This program sends a message to a queue on the RabbitMQ server.
+    Make tasks harder/longer-running by adding dots at the end of the message.
+
+    Author: Andy Asher
+    Date: May 24th, 2024
+
+"""
+
+import pika
+import sys
+import webbrowser
+import csv
+
+def offer_rabbitmq_admin_site():
+    """Offer to open the RabbitMQ Admin website"""
+    ans = input("Would you like to monitor RabbitMQ queues? y or n ")
+    print()
+    if ans.lower() == "y":
+        webbrowser.open_new("http://localhost:15672/#/queues")
+        print()
+
+def get_messages(input_file: str):
+    #create iterable messages from file
+    items = []
+    with open(input_file, "r") as input_file:
+        reader = csv.reader(input_file, delimiter="\n")
+        for row in reader:
+            items.append(row)
+    return items
+
+def send_message(host: str, queue_name: str, message: str):
+    """
+    Creates and sends a message to the queue each execution.
+    This process runs and finishes.
+
+    Parameters:
+        host (str): the host name or IP address of the RabbitMQ server
+        queue_name (str): the name of the queue
+        message (str): the message to be sent to the queue
+    """
+
+    try:
+        # create a blocking connection to the RabbitMQ server
+        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+        # use the connection to create a communication channel
+        ch = conn.channel()
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        ch.queue_declare(queue=queue_name, durable=True)
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
+        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
+        # print a message to the console for the user
+        print(f" [x] Sent {message}")
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        sys.exit(1)
+    finally:
+        # close the connection to the server
+        conn.close()
+
+def use_messages(file_name: str, host: str, queue_name: str):
+    items = get_messages(file_name)
+    for item in items:
+        message = str(list)
+        send_message(host, queue_name, message)
+        print(f"Sent: {item}")
+
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == "__main__":  
+    # ask the user if they'd like to open the RabbitMQ Admin site
+    offer_rabbitmq_admin_site()
+    #assaign file for messages
+    file_name= "tasks.csv"
+    # send messages from csv
+    use_messages(file_name, "localhost", "custom_queue")
